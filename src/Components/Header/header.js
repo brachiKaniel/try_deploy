cbafa46209ab8359ab40bc9c959fc22b36e2a112
Efrat <efratc93367@gmail.com>
2021-03-10 10:25:21 +0200

    chang2

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,26 +2,36 @@ import react from 'react';
 import './header.css'
 
 export default function Header() {
+
+
   return (
     <>
+
+
       <div className="container ">
         <div className="row  header2">
+
         </div>
+
         <div className="row header1">
           <div className="col-3">
             <div className="btn-group">
+
               <div className="dropBtn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Projects
                    </div>
-              <div className="dropContainer dropdown-menu dropdown-menu-right container ">
+
+              <div className="   dropContainer dropdown-menu dropdown-menu-right container ">
+
                 <div className="row">
                   <div className="col">
+
                   </div>
                   <div className="col itemCircle">
                     d
                      </div>
                   <div className="col-8">
-                    <button className="dropdown-item dropItem " type="button">Action1</button>
+                    <button className=" dropdown-item dropItem " type="button">Action1</button>
                   </div>
 
                 </div>
