b771d5a239be46d9303c7f5245bbd00e957a312a
Efrat <efratc93367@gmail.com>
2021-03-18 13:48:26 +0200

    can scroll the tasks up to 2 years later

--- src/Components/Gantt/displayGantt.js
+++ src/Components/Gantt/displayGantt.js
@@ -6,7 +6,7 @@ import workspaces from './workspace.json'
 
 export default function DisplayGantt() {
     debugger
-    const projectName = "project2"
+    const projectName = "project1"
     console.log(projectName);
     const allWorkspace = { workspaces };
     console.log(allWorkspace);
@@ -14,6 +14,21 @@ export default function DisplayGantt() {
 
     const theCards = []
     const theTasks = []
+    // theTasks.push(
+    //     {
+    //         "id": 16784933,
+    //         "text": "first",
+    //         "start_date": "2020-02-12",
+    //         "duration": 1,
+    //         "progress": 0.6
+    //     }
+    //     , {
+    //         "id": 2985730,
+    //         "text": "first",
+    //         "start_date": "2022-02-12",
+    //         "duration": 3,
+    //         "progress": 0.6,
+    //     })
 
     allTheWorkspaces.map((item, index) => {
         {
@@ -27,8 +42,10 @@ export default function DisplayGantt() {
     console.log("the cards", theCards);
     // console.log("now", theCards);
 
+
     {
         theCards ? theCards.map((item, index) => {
+            // theTasks.push(item.tasks)
             item.tasks.map((item, index) => {
                 console.log("oooooooppppppp", item);
                 theTasks.push(item)
@@ -36,6 +53,57 @@ export default function DisplayGantt() {
         }) : theTasks.push(null)
 
     }
+    let minYear = "3000-01-01";
+    let maxYear = "1000-01-01";
+    let currDate;
+
+    debugger
+
+    {
+        theTasks.map((item) => {
+            let year = item.start_date.split('-')[0];
+            if (year > maxYear.split('-')[0]) {
+                debugger
+                currDate = year
+                // year = parseInt(year)
+                // year = year.toString();
+                console.log("papapap", year);
+                year = year.concat('-01-01')
+                console.log("tttt", year);
+                maxYear = year;
+                // maxYear.split('-')[0] = (parseInt(year) + 1).toString();
+
+                // maxYear = year
+            } else if (year < minYear) {
+                minYear = year
+            }
+            else {
+                return
+            }
+
+
+        })
+        console.log("min", minYear);
+        console.log("max", maxYear);
+    }
+    currDate = parseInt(currDate)
+    currDate = currDate + 2
+    currDate = currDate.toString();
+    console.log("papapap", currDate);
+    currDate = currDate.concat('-01-01')
+    console.log("tttt", currDate);
+    maxYear = currDate;
+    console.log();
+    theTasks.push(
+
+        {
+            "id": 2985730,
+            "text": "first",
+            "start_date": currDate,
+            "duration": 3,
+            "progress": 0.6,
+        })
+
 
     console.log("the tasks", theTasks);
 
@@ -47,10 +115,10 @@ export default function DisplayGantt() {
         data: theTasks,
         // data: [
 
-        //     { id: 1, text: 'Task #1', start_date: "'1000-02-12'", duration: 0, progress: 0 },
-        //     { id: 2, text: 'Task #1', start_date: '2020-02-12', duration: 3, progress: 0.6, priority: "high" },
-        //     { id: 3, text: 'Task #2', start_date: '2020-04-16', duration: 3, progress: 0.4, priority: "high" },
-        //     { id: 4, text: 'Task #2', start_date: '2100-04-16', duration: 1, progress: 1 }
+        // { id: 1, text: 'Task #1', start_date: "'1000-02-12'", duration: 0, progress: 0 },
+        // { id: 2, text: 'Task #2', start_date: '2020-02-12', duration: 3, progress: 0.6, priority: "high" },
+        // { id: 3, text: 'Task #3', start_date: '2020-04-16', duration: 3, progress: 0.4, priority: "high" },
+        // { id: 4, text: 'Task #4', start_date: '2100-04-16', duration: 1, progress: 1 }
         // ],
 
         links: [
